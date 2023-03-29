87e049cfc00145e1f1747b5ceb20039f5081b800
nicklem <nicola.klemenc@codacy.com>
2023-03-29 16:50:18 +0200

    feat: Tweaks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Markprompt } from "markprompt"
 function App() {
   return (
     <div className="main">
+      <h1>The Codacy Oracle</h1>
+      <p>Ask away!</p>
       <Markprompt projectKey="4xFIDV7Nt4eNr1gXvNTKZS1bXavzEkDj" />
     </div>
   );
