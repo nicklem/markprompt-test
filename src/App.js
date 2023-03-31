1adb36ec1898c86856b1a51efd799898e9a9bb98
nicklem <nicola.klemenc@codacy.com>
2023-03-31 10:04:29 +0200

    feat: Tweaks

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="main">
       <h1>The Codacy Oracle</h1>
-      <p>Ask away!</p>
       <Markprompt
         projectKey="4xFIDV7Nt4eNr1gXvNTKZS1bXavzEkDj"
         model="gpt-4"
+        placeholder="What do you want to learn today?"
       />
     </div>
   );
